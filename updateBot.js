8b530427adbb77cfbcb74f09c7d053589ce11212
Raphael_Fonseca <raphael@fonseca.tech>
2019-08-02 12:07:35 -0400

    [updateBot.js] updated the server token fetch

--- updateBot.js
+++ updateBot.js
@@ -1,13 +1,14 @@
 const SERDEETS = require("./serverDetails.json"); // contains tokens and other server stuff
+const genStr = require("./genStr.json"); // contains bash script stuff
 const Discord = require("discord.js");
 const BOT = new Discord.Client();
 
-const genStr = require("./genStr.json"); // contains bash script stuff
+
 
 BOT.login(BOTCONF.token).then(() => {
     console.log("Bot Engaged");
     // Get GUILD ID
-    const pcrGuild = bots.guilds.get(CONST.pcrGuild['guildId']);
+    const pcrGuild = bots.guilds.get(SERDEET["pcRec"]["guildId"]);
     var pcrChannels = [];
     
     // Get list of channels in json
@@ -17,15 +18,16 @@ BOT.login(BOTCONF.token).then(() => {
         }
     }
 
-    var avChannels = []
     if (pcrGuild) {
        for (i=0;i<2;i++) {
            if (pcrGuild.channels.get(CONSTS.pcrGuild['channels'][i])); // check channels if avaliable
             avChannels.push(pcrGuild['channels'][0])
        }
 
-       if (avChannels.length) {
-           // Get new json object and display on channel
+       if (pcrChannels.length) {
+           for (i=0;i<pcrChannels.length;i++) {
+               if ()
+           }
        }
     }
     
